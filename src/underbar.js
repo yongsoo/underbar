05c1b2a34afe034e686c46ff535e62478ed61156
Yong-Soo Chung <yongsoochung@gmail.com>
2013-10-11 20:32:19 -0700

    Did not pass any tests on _.uniq function, but works when I run it

--- src/underbar.js
+++ src/underbar.js
@@ -113,13 +113,34 @@ var _ = { };
     });
 
     return result;
-    
+
     // TIP: see if you can re-use _.select() here, without simply
     // copying code in and modifying it
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+
+    result.push(array[0]);
+
+    array.forEach(function(element) {
+      for (var i = 0; i < result.length; i++) {
+        if (element !== result[i]) {
+          status = true;
+        } else {
+          status = false;
+          break;
+        }
+      }
+
+      if (status) {
+        result.push(element);
+      }
+    });
+    
+    return result;
+
   };
 
 
