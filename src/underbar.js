a45fc64e3dace2c5ffa69a317bb4eded17bacdc9
Yong-Soo Chung <yongsoochung@gmail.com>
2013-10-16 21:06:29 -0700

    _.contains function does not pass all tests, but I think I'm almost there

--- src/underbar.js
+++ src/underbar.js
@@ -215,12 +215,21 @@ var _ = { };
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function(wasFound, item) {
-      if(wasFound) {
-        return true;
-      }
-      return item === target;
-    }, false);
+    if (Array.isArray(collection)) {
+      return _.reduce(collection, function(wasFound, item) {
+        if(wasFound) {
+          return true;
+        }
+        return item === target;
+      }, false);
+    } else {
+      return _.reduce(collection, function(wasFound, item) {
+        if(wasFound) {
+          return true;
+        }
+        return Object.keys(item) === target;
+      }, false);
+    }
   };
 
 
